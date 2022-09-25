cb92d7186050038d858fb0b1e4fdc52fc84ede17
FIRAT <poyyeemobile@gmail.com>
2022-09-25 17:19:36 +0100

    battery-time

--- app/src/main/java/com/ifveral/launcher/HomeActivity.java
+++ app/src/main/java/com/ifveral/launcher/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.ifveral.launcher;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,10 +16,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomeActivity extends AppCompatActivity {
     Button btn_apps;
 
+    private TextView batteryTxt;
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            float batteryPct = level * 100 / (float)scale;
+            batteryTxt.setText(String.valueOf(batteryPct) + "%");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        batteryTxt = this.findViewById(R.id.txt_battery_level);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
         btn_apps = findViewById(R.id.btn_launcher);
         btn_apps.setOnClickListener(v -> {
